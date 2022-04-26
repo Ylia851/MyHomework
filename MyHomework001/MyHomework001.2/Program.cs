a53867eeedf25a3a1e7df4a7a45291e7774f3b2c
Yuliya <www.mksnlgzrouki@mail.ru>
2022-04-26 12:41:48 +0300

    Создали задачу 2

---
+++ MyHomework001/MyHomework001.2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+2,3,7 - 7
+44,5,78 - 78
+22,3,9 - 22
+*/
+
+Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите третье число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB > max) max = numberB;   
+if (numberC > max) max = numberC;   
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
