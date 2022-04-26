5a0b0b5c4ccbbc8f6670fabdca0867662064bd65
Yuliya <www.mksnlgzrouki@mail.ru>
2022-04-26 15:52:09 +0300

    homework001 is done

--- MyHomework001/Program.cs
+++ MyHomework001/Program.cs
@@ -13,3 +13,58 @@ if (numberA > numberB)
     Console.WriteLine("max = " + numberA);
 else
     Console.WriteLine("max = " + numberB);
+
+/*Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел
+2,3,7 - 7
+44,5,78 - 78
+22,3,9 - 22
+*/
+
+Console.WriteLine("Введите первое число");
+Console.WriteLine("Введите второе число");
+Console.WriteLine("Введите третье число");
+int numbera = Convert.ToInt32(Console.ReadLine());
+int numberb = Convert.ToInt32(Console.ReadLine());
+int numberc = Convert.ToInt32(Console.ReadLine());
+
+int max = numbera;
+
+if (numbera > max) max = numbera;
+if (numberb > max) max = numberb;   
+if (numberc > max) max = numberc;   
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+
+/*
+Напишите программу, которая на вход принимает число и выдает, является ли число четным (делится ли оно на два без остатка)
+4 - Да
+-3 - нет
+7 - нет
+*/
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number = number * (-1);
+
+int result = number%2;
+
+if (result == 0) Console.WriteLine("Число четное");
+if (result == 1) Console.WriteLine("Число нечетное");
+
+/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+5 - 2.4
+8 - 2.4.6.8
+*/
+
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while (current <= num)
+{
+    Console.WriteLine(current);
+    current = current + 2;  
+}
